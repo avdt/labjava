4a00fab1b6597799c6a8580df5f33d15b44e66b9
avdt <andriyvintoniv@ukr.net>
2015-03-10 21:06:07 +0200

    Commented

--- Home work 3/src/main/java/com/epam/Message.java
+++ Home work 3/src/main/java/com/epam/Message.java
@@ -6,24 +6,51 @@ import java.io.InputStreamReader;
 
 public class Message {
 
-	String SenderName;
+	String SenderName;	
 	String MessageContent;
+	// should be
+	// private String senderName;
+	// private String messageContent;
 
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // should be private
 
 	public String getSenderName(String SenderName) throws IOException {
 		this.SenderName = SenderName;
-		System.out.println("Èìÿ");
+		
+		// this code should be separated into another class called kinda MessagePopulator
+		System.out.println("ÃˆÃ¬Ã¿");
 		SenderName = reader.readLine();
 		return SenderName;
 
 	}
+	
+	//should be
+	/*
+	public String getSenderName(){
+		return senderName;
+	}
+	public void setSenderName(String snederName){
+		this.senderName = senderName;
+	}
+	*/
 
 	public String getMessageContent(String MessageContent) throws IOException {
 		this.MessageContent = MessageContent;
-		System.out.println("Ñîîáùåíèå");
+		
+		// this code should be separated into another class called kinda MessagePopulator
+		System.out.println("Ã‘Ã®Ã®Ã¡Ã¹Ã¥Ã­Ã¨Ã¥");
 		MessageContent = reader.readLine();
 		return MessageContent;
 	}
+	// should be
+	/*
+	public String getMessageContent(){
+		return messageContent;
+	}
+	
+	public void setMessageContent(String messageContent){
+		this.messageContent = messageContent;
+	}
+	*/
 
 }
